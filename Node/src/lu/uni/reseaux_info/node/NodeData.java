bf3034c67ffb11820a84b790dc6702d37a31e617
KREMER Michel <michel.kremer.003@student.uni.lu>
2017-05-02 17:31:55 +0200

    Added treated id list to NodeData

--- Node/src/lu/uni/reseaux_info/node/NodeData.java
+++ Node/src/lu/uni/reseaux_info/node/NodeData.java
@@ -3,8 +3,10 @@ package lu.uni.reseaux_info.node;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * Data structure which holds the mappings of key to values as well as the addresses of neighbor nodes
@@ -15,10 +17,12 @@ public class NodeData {
 	
 	private final Map<String, String> keyMap;
 	private final List<String> neighborNodes;
+	private final Set<Long> treatedIdSet;
 
 	NodeData(){
 		keyMap = Collections.synchronizedMap(new HashMap<String, String>());
 		neighborNodes = Collections.synchronizedList(new ArrayList<String>());
+		treatedIdSet = Collections.synchronizedSet(new HashSet<Long>());
 	}
 	
 	public Map<String, String> getDataMap(){
@@ -27,5 +31,9 @@ public class NodeData {
 	
 	public List<String> getNeighborAddresses(){
 		return neighborNodes;
+	}
+	
+	public Set<Long> getTreatedIdSet(){
+		return treatedIdSet;
 	}
 }
